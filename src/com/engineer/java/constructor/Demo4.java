f38e16a5ffc49537ed9b07678ac8073ebf493b07
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-07-11 12:04:50 +0530

    Add constructor overloading

---
+++ src/com/engineer/java/constructor/Demo4.java
@@ -0,0 +1,58 @@
+package com.engineer.java.constructor;
+
+public class Demo4 {
+
+    public static void main(String[] args) {
+
+        // Default constructor
+        StudentConstructorOverloading s1
+                = new StudentConstructorOverloading();
+
+        s1.display();
+
+        // Constructor with one parameter
+        StudentConstructorOverloading s2
+                = new StudentConstructorOverloading("Vivek");
+
+        s2.display();
+
+        // Constructor with two parameters
+        StudentConstructorOverloading s3
+                = new StudentConstructorOverloading("Kajal", 20);
+
+        s3.display();
+    }
+}
+
+class StudentConstructorOverloading {
+
+    String name;
+    int age;
+
+    // Default Constructor
+    StudentConstructorOverloading() {
+        System.out.println("Default Constructor Called");
+    }
+
+    // Constructor with one parameter
+    StudentConstructorOverloading(String name) {
+        this.name = name;
+        System.out.println("One Parameter Constructor Called");
+    }
+
+    // Constructor with two parameters
+    StudentConstructorOverloading(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("Two Parameter Constructor Called");
+
+    }
+
+    // Display Method
+    void display() {
+
+        System.out.println("Name : " + name);
+        System.out.println("Age  : " + age);
+        System.out.println();
+    }
+}
