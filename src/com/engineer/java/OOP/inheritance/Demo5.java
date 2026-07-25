91843a2a97a937e39c1418d12eaa69133cbb48e2
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-07-25 21:31:03 +0530

    Upcasting + Downcasting + instanceof

--- src/com/engineer/java/OOP/inheritance/Demo5.java
+++ src/com/engineer/java/OOP/inheritance/Demo5.java
@@ -3,10 +3,58 @@ package com.engineer.java.OOP.inheritance;
 public class Demo5 {
 
     public static void main(String[] args) {
-        Payment1 payment = new CreditCard1(100);
-        payment.pay();
+        Notification notification = new Email("Welcome to Java!");
+        notification.push();
 
-        Payment1 payment1 = new UPI(101);
-        payment1.pay();
+        if(notification instanceof Email){
+          Email email = (Email) notification;
+          System.out.println("Downcasting successfull.");
+        }
+
+        Notification notification2 = new SMS("OTP sent");
+        if(notification2 instanceof Email){
+          Email email = (Email) notification2;
+        }else{
+          System.out.println("notification2 is not an Email object.");
+        }
     }
+}
+
+class Notification {
+
+    protected String message;
+
+    public Notification(String message) {
+      this.message = message;
+    }
+
+    void push() {
+        System.out.println("Message Pushed");
+    }
+}
+
+class Email extends Notification {
+
+    Email(String message) {
+    super(message);
+    }
+
+    @Override
+    void push() {
+        System.out.println("Message pushed through Email.");
+    }
+
+}
+
+class SMS extends Notification {
+
+    public SMS(String message) {
+    super(message);
+    }
+
+    @Override
+    void push() {
+        System.out.println("Message pushed through SMS.");
+    }
+
 }
